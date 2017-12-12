dff1a86f309341717b0c6387a88861b676168a31
KIRIN3 <scomputer13@gmail.com>
2017-12-12 21:12:49 +0900

    Initial commit

---
+++ app/src/main/java/com/example/shinji/tabfragment/Tab1Fragment.java
@@ -0,0 +1,22 @@
+package com.example.shinji.tabfragment;
+
+/**
+ * Created by shinji on 2017/12/12.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab1Fragment extends Fragment {
+	@Override
+	public View onCreateView(
+			LayoutInflater inflater,
+			ViewGroup container,
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.tab1_fragment, container, false);
+		return v;
+	}
+}
